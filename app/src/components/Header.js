1d4e2712a8862cc8a94fc13a541312619fddd35e
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-15 16:06:34 -0800

    Updated Dependencies & Changed Application Name --> Project Completed

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => (
   <div className="header">
     <div className="container">
-      <h1 className="header__title">{props.title}</h1>
+      <h1 className="header__title">ChoiceMaker</h1>
       {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
     </div>
   </div>
